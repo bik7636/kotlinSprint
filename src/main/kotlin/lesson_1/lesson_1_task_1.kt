6114146646f6848b79c668692f7dbf909ef33161
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-01 23:43:51 +0300

    KS_1_1 delete class

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -1,8 +1,6 @@
 package lesson_1
 
-class `lesson_1_task_1` {
     fun main() {
         val numberOfOrders: Int = 75
         var thankYouMessage: String = "Большое спасибо за то что выбрали нас!"
     }
-}
