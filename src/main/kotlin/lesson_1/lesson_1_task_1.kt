ddc36a6de4b06043f87c0f506a835fbafb5c8698
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-01 22:44:21 +0300

    KS-1-1 adding the number of orders and a thank you message

---
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -0,0 +1,8 @@
+package lesson_1
+
+class `lesson_1_task_1` {
+    fun main() {
+        val numberOfOrders: Int = 75
+        var thankYouMessage: String = "Большое спасибо за то что выбрали нас!"
+    }
+}
