5f6df03e3f59c7470114d04f3ec063a9fb9d678c
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-02 19:59:31 +0300

    KS_2_1 calculation of an arithmetic expression

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,11 @@
+package lesson_2
+
+fun main() {
+    val estimation1 = 3
+    val estimation2 = 4
+    val estimation3 = 3
+    val estimation4 = 5
+
+    val result = estimation1 + estimation2 + estimation3 + estimation4 / 4f
+    println(result)
+}
