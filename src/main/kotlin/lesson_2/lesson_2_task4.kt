a98e80fc389f6581ebdad5c2de831f1370e1f150
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-03 17:49:13 +0300

    KS_2_4 calculate buff effect

---
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main() {
+    val buffEffect = 20
+   val crystalOre: Byte = 7
+    val ironOre = 11
+
+    println("Бонусной кристаллической руды: ${crystalOre * buffEffect / 100}")
+    println("Бонусной железной руды: ${ironOre * buffEffect / 100}")
+}
