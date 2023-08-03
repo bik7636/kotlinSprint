988d8ad9cdc3f79a3119328f62224e7eb15f250a
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-03 23:23:14 +0300

    KS_2_3 correcting a variable to a constant

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -1,14 +1,15 @@
 package lesson_2
 
 fun main() {
-    val departureHoursInMinutes = 9 * 60
+    val hour = 60
+    val departureHoursInMinutes = 9 * hour
     val departureInMinutes = 39
     val travelTime = 457
     val totalDepartureTime = departureHoursInMinutes + departureInMinutes
 
     val arrivalTime = (totalDepartureTime + travelTime)
-    val arrivalHours =  arrivalTime / 60
-    val arrivalMinutes = arrivalTime % 60
+    val arrivalHours =  arrivalTime / hour
+    val arrivalMinutes = arrivalTime % hour
     println("Время прибытия поезда: $arrivalHours:$arrivalMinutes")
 
 }
