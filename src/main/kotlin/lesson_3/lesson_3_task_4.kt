0bbb25419907cac2fc3a78f4dda776f789afcf23
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-04 00:50:56 +0300

    KS_3_4 syntactic sugar

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -5,12 +5,12 @@ fun main() {
     var where = "E4"
     var strokeNumber = 1
 
-    var move = "$whereFrom $where $strokeNumber"
+    var move = "$whereFrom-$where;$strokeNumber"
     println(move)
 
     whereFrom= "D2"
     where = "D3"
     strokeNumber = 1
-    move = "$whereFrom $where $strokeNumber"
+    move = "$whereFrom-$where;$strokeNumber"
     println(move)
 }
