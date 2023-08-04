2d70d771bdc5952b11249bc939f8f99043109bcc
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-04 16:01:05 +0300

    KS_2_4 syntactic sugar

--- src/main/kotlin/lesson_2/lesson_2_task4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -2,7 +2,7 @@ package lesson_2
 
 fun main() {
     val buffEffect = 20
-   val crystalOre: Byte = 7
+    val crystalOre: Byte = 7
     val ironOre = 11
 
     println("Бонусной кристаллической руды: ${crystalOre * buffEffect / 100}")
