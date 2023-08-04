9813d129cc62b7060e3fe9461c0e555b5c367532
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-04 18:20:29 +0300

    KS_4_1 comparison of data on available tables with data on tables in a restaurant

---
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -0,0 +1,10 @@
+package lesson_4
+
+fun main() {
+    val table = 13
+    var bookingToday = 13
+    var bookingTomorow = 9
+
+
+    println("Доступность столиков на сегодня: ${bookingToday != table}\nДоступность столиков на завтра: ${bookingTomorow != table}")
+}
