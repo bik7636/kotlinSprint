1603fe355d293a9a80cb04dee6d5f62acc8c93e3
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-05 17:35:51 +0300

    KS_4_3 fix remarks by constants and variables

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,11 +1,16 @@
 package lesson_4
 
 fun main() {
-    var sunyDay: Boolean = true
-    var awningOpen: Boolean = true
+    var isSunny = true
+    var isAwningOpen = true
     var airHumidity = 20
     var currentTimeYear = "зима"
-    val conditions = (sunyDay == true && awningOpen == true) && (airHumidity == 20 && currentTimeYear == "не зима")
+    val conditions = (ISSUNNY == isSunny && ISAWNINGOPEN == isAwningOpen) && (AIRHUMIDITY == airHumidity && CURRENTTIMEYEAR == currentTimeYear)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditions")
 }
+
+const val ISSUNNY = true
+const val ISAWNINGOPEN = true
+const val AIRHUMIDITY = 20
+const val CURRENTTIMEYEAR = "не зима"
