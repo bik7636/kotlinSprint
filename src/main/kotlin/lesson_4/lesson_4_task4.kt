f2234936db53c975110f6385f465927aede6116e
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-06 17:02:05 +0300

    KS_4_4 исправление реализации 2

--- src/main/kotlin/lesson_4/lesson_4_task4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task4.kt
@@ -3,15 +3,16 @@ package lesson_4
 fun main() {
 
     val day = 1
-    val isArmAndAbsDay = day % 2 == 1
-    val isLegAndBackDay = !isArmAndAbsDay
+    val isEven = day % 2 == 1
+    val isArmAndAbsDay = true
+    val isLegAndBackDay = false
 
     println(
         """
-        Упражнения для рук: $isArmAndAbsDay
-        Упражнения для ног: $isLegAndBackDay
-        Упражнения для спины: $isLegAndBackDay
-        Упражнения для пресса: $isArmAndAbsDay
+        Упражнения для рук: ${isArmAndAbsDay == isEven}
+        Упражнения для ног: ${isLegAndBackDay == isEven}
+        Упражнения для спины: ${isLegAndBackDay == isEven}
+        Упражнения для пресса: ${isArmAndAbsDay == isEven}
     """.trimIndent()
     )
 }
