41751ea71878b0161bc58e783054bcde30b8a64d
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-06 16:07:00 +0300

    KS_4_4 исправление реализации

--- src/main/kotlin/lesson_4/lesson_4_task4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task4.kt
@@ -2,19 +2,16 @@ package lesson_4
 
 fun main() {
 
-    val trainingDay = 1
-    val legsMuscles = 1
-    val armMuscles = 0
-    val backMuscles = 1
-    val pressMuscles = 0
+    val day = 1
+    val isArmAndAbsDay = day % 2 == 1
+    val isLegAndBackDay = !isArmAndAbsDay
 
-    val traning = (trainingDay % 2 >= legsMuscles && trainingDay % 2 >= backMuscles)
-    val traning2 = (trainingDay % 2 <= armMuscles && trainingDay % 2 <= pressMuscles)
-
-    println("""
-        Упражнения для рук: $traning
-        Упражнения для ног: $traning2
-        Упражнения для спины: $traning2
-        Упражнения для пресса: $traning
-    """.trimIndent())
+    println(
+        """
+        Упражнения для рук: $isArmAndAbsDay
+        Упражнения для ног: $isLegAndBackDay
+        Упражнения для спины: $isLegAndBackDay
+        Упражнения для пресса: $isArmAndAbsDay
+    """.trimIndent()
+    )
 }
