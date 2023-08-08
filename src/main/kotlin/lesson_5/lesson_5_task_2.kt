90ffd87d3d37d6f7c93a6dcd9bbd63eb121dbb9d
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-08 09:14:45 +0300

    KS_5_2 изменил алгоритм вычисления возраста пользователя

--- src/main/kotlin/lesson_5/lesson_5_task_2.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -6,12 +6,14 @@ fun main() {
 
     val calendar = Calendar.getInstance()
     val year = calendar.get(Calendar.YEAR)
-    val enteringUserAge = readln().toInt()
-    val userYearBirth = year - enteringUserAge
+    val validYearOfBirth = year - AGE_OF_MAJORITY
 
-    if (userYearBirth >= AGE_OF_MAJORITY) {
+    println("Введите Ваш год рождения: ")
+    val enteringUserAge = readln().toInt()
+
+    if (enteringUserAge <= validYearOfBirth) {
         println("Показать экран со скрытым контентом")
-    }
+    } else println("Вы слишком молод")
 }
 
 const val AGE_OF_MAJORITY = 18
