29335c60cc57f16df4bd0f562e0f09890bcdaf58
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-27 12:29:03 +0300

    KS_12_1 использование пустого конструктора

---
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -0,0 +1,34 @@
+package lesson_12
+
+fun main() {
+    val currentWeather = Weather()
+    currentWeather.dayTimeTemperature = 12
+    currentWeather.nightTimeTemperature = 9
+    currentWeather.isRain = true
+    currentWeather.atmosphericPressure = 350
+    currentWeather.WeatherData()
+
+    val currentWeather2 = Weather()
+    currentWeather2.dayTimeTemperature = 29
+    currentWeather2.nightTimeTemperature = 19
+    currentWeather2.isRain = false
+    currentWeather2.atmosphericPressure = 679
+    currentWeather2.WeatherData()
+
+
+}
+
+class Weather() {
+    var dayTimeTemperature = 30
+    var nightTimeTemperature = 18
+    var isRain = false
+    var atmosphericPressure = 739
+
+    fun WeatherData() {
+        println(
+            "is weather today: t day = $dayTimeTemperature," +
+                    " t night = $nightTimeTemperature, rain = $isRain, atmos.pressure = $atmosphericPressure"
+        )
+    }
+
+}
