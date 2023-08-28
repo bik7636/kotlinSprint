088457607bea4f79aab3b5991a54c34071f27bcc
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-28 10:35:51 +0300

    KS_12_1 правки по рекомендациям

--- src/main/kotlin/lesson_12/lesson_12_task_1.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -6,25 +6,25 @@ fun main() {
     currentWeather.nightTimeTemperature = 9
     currentWeather.isRain = true
     currentWeather.atmosphericPressure = 350
-    currentWeather.WeatherData()
+    currentWeather.showWeather()
 
     val currentWeather2 = Weather()
     currentWeather2.dayTimeTemperature = 29
     currentWeather2.nightTimeTemperature = 19
     currentWeather2.isRain = false
     currentWeather2.atmosphericPressure = 679
-    currentWeather2.WeatherData()
+    currentWeather2.showWeather()
 
 
 }
 
-class Weather() {
+class Weather {
     var dayTimeTemperature = 30
     var nightTimeTemperature = 18
     var isRain = false
     var atmosphericPressure = 739
 
-    fun WeatherData() {
+    fun showWeather() {
         println(
             "is weather today: t day = $dayTimeTemperature," +
                     " t night = $nightTimeTemperature, rain = $isRain, atmos.pressure = $atmosphericPressure"
