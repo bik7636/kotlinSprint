def11409d24eda0f487e393766dbd38b9eb517ed
aleksa.boyko <aleksa.boyko@x5.ru>
2023-08-28 10:26:45 +0300

    KS_11_3 правки по рекомендациям

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -1,33 +1,31 @@
 package lesson_11
 
 fun main() {
-    val room1 = Room(
-        1,
-        "android.jpg",
-        "kotlin lovers",
-        listOf(User(1, "avatar1", "someUser2")),
-        Status(false, true, true)
+    val room = Room(
+        1, "kotlin.jpg", "kotlin lovers",
+        listOf(User(1, "avatar1", "someUser1", false, true, true))
     )
 
     val room2 = Room(
-        2,
-        "cat.jpg",
-        "cat lovers",
-        listOf(User(2, "avatar2", "someUser2")),
-        Status(true, false, false)
+        2, "cat.jpg", "cat lovers",
+        listOf(User(2, "avatar2", "someUser2", true, false, false))
     )
 
-    val rooms = listOf(room1, room2)
+    val rooms = listOf(room, room2)
 }
 
-class Room(val id: Int, val cover: String, val title: String, val users: List<User>, status: Status) {
+class Room(
+    val id: Int,
+    val cover: String,
+    val title: String,
+    val users: List<User>
+)
 
-}
-
-class User(val id: Int, val avatar: String, val nikname: String) {
-
-}
-
-class Status(val isTalking: Boolean, val isMicrophoneOff: Boolean, val isSilenced: Boolean) {
-
-}
+class User(
+    val id: Int,
+    val avatar: String,
+    val nikname: String,
+    val isTalking: Boolean,
+    val isMicrophoneOff: Boolean,
+    val isSilenced: Boolean
+)
