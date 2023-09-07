7b18cd2bf8c60b6a8234279e771cf5878babd498
aleksa.boyko <aleksa.boyko@x5.ru>
2023-09-07 22:41:51 +0300

    20_2 2-я попытка подружиться с лямбдой

--- src/main/kotlin/lesson_20/lesson_20_task_2.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_2.kt
@@ -10,7 +10,7 @@ fun main() {
     }
 
     healingPotion(20)
-    println("Текущее здоровье: ${player.currentHealth}")
+    println("Текущее здоровье: ${player.currentHealth}" )
 }
 
 class Player(val name: String, var currentHealth: Int, val maxHealth: Int)
